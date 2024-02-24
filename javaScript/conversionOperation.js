379875d6460c93ed1b16e73a626e35f557637842
LalbabuG <140197112+LalbabuG@users.noreply.github.com>
2024-02-24 18:56:05 +0000

    conversion of data type

---
+++ javaScript/conversionOperation.js
@@ -0,0 +1,65 @@
+// let score=33
+// console.log(typeof score)  //o/p--Number
+
+// let num="33abs"
+// console.log(typeof num)    //o/p--String
+/**
+ * convert a string in number
+ */
+// let valueInNumber=Number(num)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//Note--
+// conversion
+// "33"->33
+// "33abc"->NaN
+// true->1
+
+// let loggedIn=0;
+
+// let booleanLoggedIn=Boolean(loggedIn)
+// console.log(booleanLoggedIn)
+
+// 1->true
+// 0->false
+// "Lalbabu"->true
+// ""->false
+
+                  // String
+//  let num1=33
+
+//  let stringNum=String(num1)
+//  console.log(stringNum)
+//  console.log(typeof stringNum)
+
+//  ********************Operations*********************
+
+let value=3
+let negValue=-value
+console.log(negValue)  //o/p->-3
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)   //2^3
+// console.log(2/2)
+// console.log(2%2)
+
+let str="Lalbabu"
+let str1="kumar"
+
+let str3=str+str1
+console.log(str3)
+
+// console.log("1" + 2)   //o/p=>12
+// console.log(1+"2")     //o/p=>12
+// console.log("1"+2+2)  //o/p=>122
+// console.log(1+2+"2")   //o/p=>32
+
+console.log(+true)
+
+
+
+
+
