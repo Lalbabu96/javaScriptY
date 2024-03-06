e3380e26376ee70bce02b4dfc53cb25b871b51e6
LalbabuG <140197112+LalbabuG@users.noreply.github.com>
2024-03-06 17:41:39 +0000

    jason and more object

---
+++ javaScript/Object/object3.js
@@ -0,0 +1,20 @@
+
+const course ={
+    courseName :"javaScript",
+    price:"999",
+    courseInstructor :"YouTube"
+}
+
+// course.courseInstructor
+
+const {courseInstructor}=course
+console.log(courseInstructor)
+
+// JASON
+// {
+//     "name":"lalbabu",
+//     courseName : "javaScript",
+//     price :"pay as you go"
+// }
+
+
