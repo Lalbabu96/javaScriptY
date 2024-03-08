0b991db5a20f77ac418ef7b34c7310d457ec788c
Lalbabu Kumar <140197112+Lalbabu96@users.noreply.github.com>
2024-03-08 18:23:33 +0000

    function concept

---
+++ javaScript/Function/function1.js
@@ -0,0 +1,31 @@
+
+
+function myName(){
+    console.log("L")
+    console.log("A")
+    console.log("L")
+    console.log("B")
+    console.log("A")
+    console.log("B")
+    console.log("U")
+}
+// myName()
+
+function addTwoNumber(num1,num2){
+    console.log (num1+num2)
+}
+
+
+// addTwoNumber(5,9)
+
+function addTwoNumber(num1,num2){
+    return num1+num2
+}
+ const result =addTwoNumber(5,9)
+//  console.log(result);
+
+function loginUserMessage(userName){
+    return `${userName} just logged in`
+}
+console.log(loginUserMessage("vikash"));
+
